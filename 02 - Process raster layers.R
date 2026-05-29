00d586be35190582ca27a644732c09444a017cc7
nhooven <nathan.d.hooven@gmail.com>
2026-05-29 12:31:42 -0700

    Set up scripts for rest site modeling

---
+++ 02 - Process raster layers.R
@@ -0,0 +1,14 @@
+# PROJECT: Rest site selection
+# SCRIPT: 02 - Process raster layers
+# AUTHOR: Nate Hooven
+# EMAIL: nathan.d.hooven@gmail.com
+# BEGAN: 29 May 2026
+# COMPLETED: 
+# LAST MODIFIED: 
+# R VERSION: 4.5.2
+
+# ______________________________________________________________________________
+# 1. Load packages ----
+# ______________________________________________________________________________
+
+library(tidyverse)
